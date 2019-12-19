d4a3f58c1c6e285b540f8957bc3dc1495b6b86c4
Balabek <vonbabayan@gmail.com>
2019-12-19 22:48:10 +0300

    Final 19.12.2019

--- js/singermarketing.js
+++ js/singermarketing.js
@@ -7,21 +7,30 @@ $(document).ready(function(){
         infinite: false,
         speed: 300,
         auto: true,
-        slidesToShow: 5,
+        slidesToShow: 6,
         slidesToScroll: 1,
         responsive: [
             {
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 3,
+                    slidesToShow: 4,
                     slidesToScroll: 1,
                     infinite: true,
                     dots: true
                 }
             },
             {
+                breakpoint: 767,
+                settings: {
+                    dots: false,
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
                 breakpoint: 600,
                 settings: {
+                    dots: false,
                     slidesToShow: 2,
                     slidesToScroll: 1
                 }
@@ -29,6 +38,7 @@ $(document).ready(function(){
             {
                 breakpoint: 480,
                 settings: {
+                    dots: false,
                     slidesToShow: 1,
                     slidesToScroll: 1
                 }
